7b3480345dd2ed8c6b9469a4299676a606777af8
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-07-14 01:00:57 -0300

    feat: criacao de menu de selecao de api a ser consumida

--- src/main/java/org/estudo/Main.java
+++ src/main/java/org/estudo/Main.java
@@ -3,21 +3,40 @@ package org.estudo;
 
 import org.estudo.coinGeckoApi.service.CoinGeckoApiService;
 import org.estudo.coinGeckoApi.service.ICoinGeckoApiService;
+import org.estudo.googleBooksApi.service.GoogleBooksApiService;
+import org.estudo.googleBooksApi.service.IGoogleBooksApiService;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    //final static IGoogleBooksApiService googleBooksApiService = new GoogleBooksApiService();
+    final static IGoogleBooksApiService googleBooksApiService = new GoogleBooksApiService();
     final static ICoinGeckoApiService coinGeckoApiService = new CoinGeckoApiService();
 
     public static void main(String[] args) throws IOException, InterruptedException {
         final var scanner = new Scanner(System.in);
-        final var query = scanner.nextLine();
 
-        //googleBooksApiService.findBookByName(query);
-        coinGeckoApiService.findCoinById(query);
+        System.out.println("Selecione o tipo de servico api que deseja consumir: ");
+        System.out.println("1 - Google Books");
+        System.out.println("2 - Coin Gecko");
+        System.out.println(">>");
+        final var opcao = scanner.nextLine();
 
+        switch (opcao) {
+            case "1":
+                System.out.println("Escreva o nome do livro:\t");
+                final var nomeLivro = scanner.nextLine();
+                googleBooksApiService.findBookByName(nomeLivro);
+                break;
+            case "2":
+                System.out.println("Escreva o id da coin:\t (conforme /coins/list da documentacao)");
+                final var idMoeda = scanner.nextLine();
+                coinGeckoApiService.findCoinById(idMoeda);
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
     }
 
 }
