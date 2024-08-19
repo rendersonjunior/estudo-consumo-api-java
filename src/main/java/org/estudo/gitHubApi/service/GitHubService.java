a01f14bf948079a50527ea6f7478268cf3a0344f
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-08-18 21:43:24 -0300

    feat: inicio da criacao de service para busca no github

---
+++ src/main/java/org/estudo/gitHubApi/service/GitHubService.java
@@ -0,0 +1,11 @@
+package org.estudo.gitHubApi.service;
+
+public class GitHubService implements IGitHubService {
+
+    @Override
+    public void findUserByName(final String username) {
+        System.out.println("Consultando usuário do GitHub...");
+
+    }
+
+}
---
+++ src/main/java/org/estudo/gitHubApi/service/IGitHubService.java
@@ -0,0 +1,7 @@
+package org.estudo.gitHubApi.service;
+
+public interface IGitHubService {
+
+    void findUserByName(final String username);
+
+}
