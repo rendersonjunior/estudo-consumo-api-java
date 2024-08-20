b0841361fc7df9e5ff41b53388b6d64f09517af1
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-08-20 11:34:17 -0300

    feat: criacao de record para retornar json do github

--- src/main/java/org/estudo/gitHubApi/model/UserGit.java
+++ src/main/java/org/estudo/gitHubApi/model/UserGit.java
@@ -1,4 +1,38 @@
 package org.estudo.gitHubApi.model;
 
-public record User() {
-}
+import com.google.gson.annotations.SerializedName;
+
+public record UserGit(
+        String login,
+        Integer id,
+        @SerializedName("node_id") String nodeId,
+        @SerializedName("avatar_url") String avatarUrl,
+        @SerializedName("gravatar_id") String gravatarId,
+        String url,
+        @SerializedName("html_url") String htmlUrl,
+        @SerializedName("followers_url") String followersUrl,
+        @SerializedName("following_url") String followingUrl,
+        @SerializedName("gists_url") String gistsUrl,
+        @SerializedName("starred_url") String starredUrl,
+        @SerializedName("subscriptions_url") String subscriptionsUrl,
+        @SerializedName("organizations_url") String organizationsUrl,
+        @SerializedName("repos_url") String reposUrl,
+        @SerializedName("events_url") String eventsUrl,
+        @SerializedName("received_events_url") String receivedEventsUrl,
+        String type,
+        @SerializedName("site_admin") Boolean siteAdmin,
+        String name,
+        String company,
+        String blog,
+        String location,
+        String email,
+        String hireable,
+        String bio,
+        @SerializedName("twitter_username") String twitterUsername,
+        @SerializedName("public_repos") Integer publicRepos,
+        @SerializedName("public_gists") Integer publicGists,
+        Integer followers,
+        Integer following,
+        @SerializedName("created_at") String createdAt,
+        @SerializedName("updated_at") String updatedAt
+) {}
