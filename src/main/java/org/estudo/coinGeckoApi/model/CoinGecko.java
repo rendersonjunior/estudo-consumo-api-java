ddf7f20509e8e82bd8f35c87cad6796aeb595940
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-08-22 00:12:19 -0300

    feat: implementacao do gerar arquivo txt

--- src/main/java/org/estudo/coinGeckoApi/model/CoinGecko.java
+++ src/main/java/org/estudo/coinGeckoApi/model/CoinGecko.java
@@ -1,6 +1,23 @@
 package org.estudo.coinGeckoApi.model;
 
+import org.estudo.util.IGerarArquivo;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
-public record CoinGecko(Map<String, ValorMoeda> moedas) {
+public record CoinGecko(Map<String, ValorMoeda> moedas) implements IGerarArquivo {
+
+    @Override
+    public void gerarArquivoTxt(String tituloArquivo) {
+        try (final var fileWriter = new FileWriter("/src/tmp/".concat(tituloArquivo.concat(".txt")))) {
+
+            for (final var moeda : moedas.entrySet()) {
+                fileWriter.write(moeda.getKey().concat("= ".concat(moeda.getValue().toString())));
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Erro ao gerar Arquivo: ".concat(ex.getMessage()));
+        }
+    }
 }
